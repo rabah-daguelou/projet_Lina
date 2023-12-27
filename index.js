30b2519dc19c27b02dcd40983cbd085ce4f3e97f
lina <lin.dag@yahoo.com>
2023-12-27 11:13:42 +0100

    forth commit

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-
 // Menu Burger
 icons.addEventListener("click", () => {
   nav.classList.toggle("active");
-    //divSlider.classList.toggle("slider");
-    divSliderAndFilmsFound.classList.toggle("slider")
+  //divSlider.classList.toggle("slider");
+  divSliderAndFilmsFound.classList.toggle("slider");
 });
 
-let films = document.getElementById("films");
+let films = document.getElementById("myFilms");
+let series = document.getElementById("mySeries");
+let docs = document.getElementById("myDocs");
+let autres = document.getElementById("myAutres");
+
 // Liste des films
 let myFilms = [
   "james.jpg",
-  "zinder.jpg",
   "alice.jpg",
-
   "athena.jpg",
   "charlie.jpeg",
-  "mercredi.jpg",
+  "grenouille.jpg",
+  "harry.jpg",
+  "pirates.jpg",
+  "raiponce.jpg",
+  "avatar.jpg",
+  "prince.jpg",
 ];
 let mySeries = [
-  "chucky.jpg",
-  "conjuring.jpg",
-  "grita.jpg",
-  
-  "lupin.jpg",
-  
-];
-let myDocumentaires = [
   "monk.jpg",
- 
- 
-  "predateur.jpg",
- 
-  
+  "lupin.jpg",
+  "mercredi.jpg",
   "riverdale.jpg",
+  "casa.jpeg",
+  "columbo.jpg",
+  "ginny.jpg",
+  "desperate.jpg",
+  "gossip.jpg",
+  "stranger.jpg",
+];
+let myDocs = [
+  "grita.jpg",
+  "zinder.jpg",
+  "predateur.jpg",
+  "britney.jpg",
+  "hitler.jpg",
+  "maradona.jpg",
+  "poutine.jpg",
+  "jackson.jpg",
+  "marie.jpg",
+  "rome.jpg",
 ];
 let myAutres = [
-  "riverdale.jpg",
+  "chucky.jpg",
+  "aimes.jpg",
+  "ca.jpg",
+  "elle.jpg",
+  "proie.jpg",
+  "conjuring.jpg",
   "smile.jpeg",
-  
+  "annabelle.jpg",
+  "exorcisme.jpg",
+  "nonne.jpg",
 ];
 
+let myDivs = [films, series, docs, autres];
+let AllLists = [myFilms, mySeries, myDocs, myAutres];
+
 // Créer les boutons gauche / Droit
-let slideLeftDiv = document.createElement("div");
+/*let slideLeftDiv = document.createElement("div");
 slideLeftDiv.innerHTML = "&lt";
 slideLeftDiv.classList = "slide_left";
 slideLeftDiv.setAttribute("onClick", "slideTo(-1)");
@@ -50,8 +73,9 @@ let slideRightDiv = document.createElement("div");
 slideRightDiv.innerHTML = "&gt";
 slideRightDiv.classList = "slide_right";
 slideRightDiv.setAttribute("onClick", "slideTo(1)");
-
+*/
 // Fonction de slide
+/*
 function injectFilms() {
   films.appendChild(slideLeftDiv);
   films.appendChild(slideRightDiv);
@@ -62,22 +86,63 @@ function injectFilms() {
     films.append(imageTag);
   });
 }
+*/
+
+// nouveau code
+function injectFilms() {
+  let arguments = ["myFilms", "mySeries", "myDocs", "myAutres"];
+  let n = 0;
+  myDivs.forEach((myDiv) => {
+    let slideLeftDiv = document.createElement("div");
+    slideLeftDiv.innerHTML = "&lt";
+    slideLeftDiv.classList = "slide_left";
+
+    slideLeftDiv.setAttribute("onClick", `slideTo(-1,${n})`);
+
+    let slideRightDiv = document.createElement("div");
+    slideRightDiv.innerHTML = "&gt";
+    slideRightDiv.classList = "slide_right";
+    slideRightDiv.setAttribute("onClick", `slideTo(1, ${n})`);
+
+    myDiv.appendChild(slideLeftDiv);
+    myDiv.appendChild(slideRightDiv);
+
+    AllLists[n].forEach((image) => {
+      if (n > AllLists[n].length) {
+        n == AllLists[n].length;
+      }
+      let imageTag = document.createElement("img");
+      imageTag.src = "images/" + image;
+      myDiv.append(imageTag);
+    });
+    n++;
+  });
+}
+
+// Fin nouveau code
+///
+
 injectFilms();
 
 // Slide_left & Slide_right
 
-function slideTo(sens) {
+function slideTo(sens, n) {
+  let liste = AllLists[n];
+  let div = myDivs[n];
+
+  console.log(liste);
+  console.log(div);
   if (sens == -1) {
-    let first_image = myFilms.shift();
-    myFilms.push(first_image);
-    films.innerHTML = "";
+    let first_image = liste.shift();
+    liste.push(first_image);
+    div.innerHTML = "";
   } else {
-    let last_image = myFilms[myFilms.length - 1];
-    myFilms.pop();
-    myFilms.unshift(last_image);
-    films.innerHTML = "";
+    let last_image = liste[liste.length - 1];
+    liste.pop();
+    liste.unshift(last_image);
+    div.innerHTML = "";
   }
-  injectFilms();
+  injectFilms(); /**/
 }
 
 // Chercher un film
@@ -94,83 +159,66 @@ h2.textContent = "Films trouvés ";
 let divSliderAndFilmsFound = document.getElementById("divSliderAndFilmsFound");
 
 // Message Pas de films trouvés
-let message = document.createElement('p')
-message.setAttribute('id', 'message')
-    message.textContent = "Aucun film ne correspond à votre recherche!";
+let message = document.createElement("p");
+message.setAttribute("id", "message");
+message.textContent = "Aucun film ne correspond à votre recherche!";
 // Injecter la div des films trouvés si input a une valeur
 myInput.addEventListener("input", () => {
   let liste = [];
   divFilmsFound.innerHTML = "";
-        
-        // div des images
-        let divFilmsFoundImages = document.createElement("div");
-        divFilmsFoundImages.setAttribute("id", "divFilmsFoundImages");
 
-        divSliderAndFilmsFound.appendChild(divFilmsFound);
-    divFilmsFound.appendChild(h2);
+  // div des images
+  let divFilmsFoundImages = document.createElement("div");
+  divFilmsFoundImages.setAttribute("id", "divFilmsFoundImages");
+
+  divSliderAndFilmsFound.appendChild(divFilmsFound);
+  
+
+  // Chercher les films correspondants
+
+  AllLists.forEach((element) => {
+    element.forEach((film) => {
+      if (film.startsWith(myInput.value)) {
+        liste.push(film);
+      }
+    });
     
-// Chercher les films correspondants
-        myFilms.forEach((film) => {
-            if (film.startsWith(myInput.value)) {
-                liste.push(film);
-            } 
-        });
-        mySeries.forEach((film) => {
-          if (film.startsWith(myInput.value)) {
-            liste.push(film);
-          }
-        });
-        myDocumentaires.forEach((film) => {
-          if (film.startsWith(myInput.value)) {
-            liste.push(film);
-          }
-        });
-        myAutres.forEach((film) => {
-          if (film.startsWith(myInput.value)) {
-            liste.push(film);
-          }
-        });
-    
-// Si des films trouvés   
+        // Si des films trouvés
     if (myInput.value && liste.length) {
-        divFilmsFoundImages.innerHTML = ''
-        
-        liste.forEach((image) => {
-                
-                divFilmsFound.appendChild(divFilmsFoundImages);
-               // divFilmsFoundImages.appendChild(filmImage);
-            //
-            let divImage = document.createElement('div')
-            divFilmsFoundImages.appendChild(divImage);
-            divImage.setAttribute('id', 'divImage')
-            //
-                let filmImage = document.createElement("img");
-            filmImage.setAttribute("id", "imageFound");
-            
-            filmImage.src = `images/${image}`;
-            //
-            let h4 = document.createElement('h4')
-            h4.style.textAlign = 'center'
-            
-            h4.textContent = image.split('.')[0].toUpperCase()
+      divFilmsFoundImages.innerHTML = "";
+        message.innerHTML=''
+        divFilmsFound.appendChild(h2);
+      liste.forEach((image) => {
+        divFilmsFound.appendChild(divFilmsFoundImages);
+        let divImage = document.createElement("div");
+        divFilmsFoundImages.appendChild(divImage);
+        divImage.setAttribute("id", "divImage");
+        //
+        let filmImage = document.createElement("img");
+        filmImage.setAttribute("id", "imageFound");
 
-            
+        filmImage.src = `images/${image}`;
+        //
+        let h4 = document.createElement("h4");
+        h4.style.textAlign = "center";
 
-            divImage.appendChild(filmImage)
-            divImage.appendChild(h4)
-            
-            
-        });
+        h4.textContent = image.split(".")[0].toUpperCase();
 
-// Si aucun film trouvé        
-    } else if (liste.length == 0) {
-        divFilmsFound.innerHTML =''
-        divFilmsFound.append(message)
-        return
-    }
-// Si input vidé    
-    else {
+        divImage.appendChild(filmImage);
+        divImage.appendChild(h4);
+      });
+
+      // Si aucun film trouvé
+    } else if (liste.length <1) {
+        console.log(liste);
+        divFilmsFound.innerHTML = "";
+        message.textContent = "Aucun film ne correspond à votre recherche!";
+      divFilmsFound.appendChild(message);
+      return;
+      // Si input vidé
+    } else {
         divFilmsFound.remove();
+        message.textContent = "";
     }
+  });
 });
-
